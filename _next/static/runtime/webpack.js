11b04e681ddb4aa0bdb5e12d8b4adb7b5396a669
Grant Custer <grantcuster@gmail.com>
2019-04-28 09:17:07 -0400

    Deploy Next.js to gh-pages

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "53b70ce8c12371936bd0";
+/******/ 	var hotCurrentHash = "487532bac65f41902e6d";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
---
+++ _next/static/webpack/static/development/pages/index.js.082d2c6303092fe3ee92.hot-update.js
@@ -0,0 +1,15 @@
+webpackHotUpdate("static/development/pages/index.js",{
+
+/***/ "./data/experiments.json":
+/*!*******************************!*\
+  !*** ./data/experiments.json ***!
+  \*******************************/
+/*! exports provided: experiments, default */
+/***/ (function(module) {
+
+module.exports = {"experiments":[{"name":"test","address":"test"}]};
+
+/***/ })
+
+})
+//# sourceMappingURL=index.js.082d2c6303092fe3ee92.hot-update.js.map
