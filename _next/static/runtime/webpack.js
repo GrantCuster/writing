f885cf971026a23406fb8aae4205513101bdb8a7
Grant Custer <grantcuster@gmail.com>
2019-06-23 20:41:14 -0400

    Deploy Next.js to gh-pages

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -112,7 +112,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "bc43484500662a8e58de";
+/******/ 	var hotCurrentHash = "b4118a84f32d687bf1f1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
